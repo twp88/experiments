82d9bdfa46de6ee58afe6f7a4784da82d86a1d7f
Thomas William Page <thomaswilliampage@Thomass-MBP.home>
2016-12-27 12:57:20 +0000

    all commented out

--- lib/thing.rb
+++ lib/thing.rb
@@ -3,9 +3,9 @@ class Thing
   attr_reader :bups, :nonsense, :stuff
 
   # This is quite clearly a poor example of overcoming the scope issues posed by
-  ruby. The dependencies here create really bad code smell. Class Thing HAS to
-  know what Thang is, and hence needs it to be able to get to all of the variables
-  it wants. 
+  # ruby. The dependencies here create really bad code smell. Class Thing HAS to
+  # know what Thang is, and hence needs it to be able to get to all of the variables
+  # it wants.
 
   def initialize
     @bups = Thang.new
